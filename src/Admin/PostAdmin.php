75c2de780d92ed5be75a818d55f0130f280484c8
AdolfoAugustoR <adolfoaugusto2010@hotmail.com>
2019-03-19 22:16:37 -0300

    finalizado, ajustes no form de posts, login logo e name do sistema.

--- src/Admin/PostAdmin.php
+++ src/Admin/PostAdmin.php
@@ -11,11 +11,14 @@ use App\Entity\Author;
 use App\Entity\Category;
 use App\Entity\Post;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\ModelType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use function Symfony\Component\Validator\Tests\Constraints\choice_callback;
 
 class PostAdmin extends AbstractAdmin
 {
@@ -37,22 +40,42 @@ class PostAdmin extends AbstractAdmin
         ]);
     }
 
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter->add('title')
+        ->add('category', null, [], EntityType::class, [
+            'class'        => Category::class,
+            'choice_label' => 'name'
+        ])
+        ->add('status')
+        ->add('author.name');
+    }
+
     protected function configureFormFields(FormMapper $form)
     {
-        $form->add('category', ModelType::class,[
-            'class' => Category::class,
-            'property' => 'name',
-            'multiple' => true
-        ])
-            ->add('author', ModelType::class,[
-                'class' => Author::class,
-                'property' => 'name'
-            ])
-            ->add('title', TextType::class)
-            ->add('content', TextareaType::class)
-            ->add('status', null, [
-                'required' => false
-            ]);
+        $form
+            ->tab('Conteúdo')
+                ->with('Conteúdo')
+                    ->add('title', TextType::class)
+                    ->add('content', TextareaType::class)
+                    ->add('status', null, [
+                        'required' => false
+                    ])
+                ->end()
+            ->end()
+            ->tab('Auxiliar')
+                ->with('Auxiliar')
+                    ->add('category', ModelType::class,[
+                    'class' => Category::class,
+                    'property' => 'name',
+                    'multiple' => true
+                    ])
+                    ->add('author', ModelType::class,[
+                        'class' => Author::class,
+                        'property' => 'name'
+                    ])
+                ->end()
+            ->end();
     }
 
     public function toString($object)
